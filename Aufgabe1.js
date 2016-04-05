512763da38ab1132f8cd2b2eca6ba00f4d495886
Steffen Owtschinnikow <s_owtschinnikow@gmx.de>
2016-04-06 00:38:23 +0200

    Aufgabe1

--- Aufgabe1.js
+++ Aufgabe1.js
@@ -1,12 +1,15 @@
 var fs = require('fs');
+var chalk = require('chalk');
 
 fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data){
 	var kratzer=JSON.parse(data.toString());
-
+	kratzer.wolkenkratzer.sort(function(a,b){
+		return a.hoehe > b.hoehe;
+   });
 	for (var i=0; i<kratzer.wolkenkratzer.length; i++) {
-		console.log("Name: "+kratzer.wolkenkratzer[i].name);
-		console.log("Stadt: "+kratzer.wolkenkratzer[i].stadt);
-		console.log("Hoehe: "+kratzer.wolkenkratzer[i].hoehe);
+		console.log(chalk.blue("Name: ")+chalk.yellow(kratzer.wolkenkratzer[i].name));
+		console.log(chalk.magenta("Stadt: ")+chalk.green(kratzer.wolkenkratzer[i].stadt));
+		console.log(chalk.cyan("Hoehe: ")+chalk.gray(kratzer.wolkenkratzer[i].hoehe));
 		console.log("----------------");
 	};
 });
