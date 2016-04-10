f5e58b3fb9aa073a6a4697f556a665aa3c618f55
Steffen Owtschinnikow <s_owtschinnikow@gmx.de>
2016-04-10 21:42:07 +0200

    Aufgabe1

--- Aufgabe1.js
+++ Aufgabe1.js
@@ -3,9 +3,10 @@ var chalk = require('chalk');
 
 fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data){
 	var kratzer=JSON.parse(data.toString());
-	kratzer.wolkenkratzer.sort(function(a,b){
+		kratzer.wolkenkratzer.sort(function(a,b){
 		return a.hoehe > b.hoehe;
    });
+fs.writeFile(__dirname+"/wolkenkratzer_sortiert.json", JSON.stringify(kratzer), function(err) {console.log("Gesichert!")});
 	for (var i=0; i<kratzer.wolkenkratzer.length; i++) {
 		console.log(chalk.blue("Name: ")+chalk.yellow(kratzer.wolkenkratzer[i].name));
 		console.log(chalk.magenta("Stadt: ")+chalk.green(kratzer.wolkenkratzer[i].stadt));
